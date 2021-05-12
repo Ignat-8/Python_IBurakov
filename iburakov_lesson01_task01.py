b1d2ccfb0e89c663a1e3a46f9b4f3aaeafac1d4c
Ignat <iburako8@yandex.ru>
2021-05-12 22:31:21 +0300

    внес изменения в синтаксис согласно PEP-8

--- iburakov_lesson01_task01.py
+++ iburakov_lesson01_task01.py
@@ -1,17 +1,19 @@
 # Подготовка входных данных ------------------------------------------------
-duration = 0
-while duration <= 0:
+while True:
     duration = int(input("Введите продолжительность времени в секундах:"))
-    if duration < 0:
+    if duration <= 0:
         print("число должно быть больше нуля")
+    else:
+        break
+
 # Расчет промежутков времени -----------------------------------------------
 format_out = ['сек.', 'мин.', 'час.', 'дн.', 'лет']
 denominators = [60, 60, 24, 365]
-list_out = [] #список выходных данных
+list_out = []  # список выходных данных
 
 for i in range(len(format_out)):
     if duration > 0:
-        if i < len(format_out)-1:
+        if i < len(format_out) - 1:
             list_out.append([str(duration % denominators[i]), format_out[i]])
             duration = duration // denominators[i]
         else:
@@ -20,6 +22,7 @@ for i in range(len(format_out)):
 message_out = ""
 len_list_out = len(list_out) - 1
 for i in range(len_list_out + 1):
-    message_out += list_out[len_list_out - i][0] + " " + list_out[len_list_out - i][1] + " "
-#-------------------------------------------------------------------------
+    message_out += list_out[len_list_out - i][0] + " "
+    message_out += list_out[len_list_out - i][1] + " "
+# -------------------------------------------------------------------------
 print(message_out)
