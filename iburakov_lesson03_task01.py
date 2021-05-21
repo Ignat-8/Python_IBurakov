74f570a33bd04ecb853038be2476a7903122d54c
Ignat <iburako8@yandex.ru>
2021-05-21 21:36:58 +0300

    сделал 4-е задание

--- iburakov_lesson03_task01.py
+++ iburakov_lesson03_task01.py
@@ -8,30 +8,38 @@
 # Подумайте, как и где лучше хранить информацию, необходимую для перевода:
 # какой тип данных выбрать, в теле функции или снаружи.
 
-num_translate = {
-    'one': 'один',
-    'two': 'два',
-    'three': 'три',
-    'four': 'четыре',
-    'five': 'пять',
-    'six': 'шесть',
-    'seven': 'семь',
-    'eight': 'восемь',
-    'nine': 'девять',
-    'ten': 'десять',
-    'один': 'one',
-    'два': 'two',
-    'три': 'three',
-    'четыре': 'four',
-    'пять': 'five',
-    'шесть': 'six',
-    'семь': 'seven',
-    'восемь': 'eight',
-    'девять': 'nine',
-    'десять': 'ten'
-}
 
-english_number = input('Введите числительное для перевода на русский\английский или q для выхода: ')
-while english_number != 'q':
-    print(num_translate.get(english_number))
-    english_number = input('Введите числительное для перевода на русский\английский или q для выхода: ')
+def num_translate(world):
+    """ Перевод русских и английских числительных """
+    enumerate_dict = {
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'five': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять',
+        'ten': 'десять',
+        'один': 'one',
+        'два': 'two',
+        'три': 'three',
+        'четыре': 'four',
+        'пять': 'five',
+        'шесть': 'six',
+        'семь': 'seven',
+        'восемь': 'eight',
+        'девять': 'nine',
+        'десять': 'ten'
+        }
+    if enumerate_dict.get(world):
+        return f'Перевод {world} - {enumerate_dict.get(world)}'
+    else:
+        return None
+
+
+world_translate = input('Введите числительное для перевода на русский\английский или q для выхода: ')
+while world_translate != 'q':
+    print(num_translate(world_translate))
+    world_translate = input('Введите числительное для перевода на русский\английский или q для выхода: ')
